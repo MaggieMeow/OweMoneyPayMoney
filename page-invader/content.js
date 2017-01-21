0c4f3618197cbe83cbfbda041b73cfdd41b7520e
LookRain <brantscube@hotmail.com>
2017-01-21 16:55:12 +0800

    got the gravity extension working

--- page-invader/content.js
+++ page-invader/content.js
@@ -2,3 +2,11 @@ var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
+
+var s = document.createElement('script');
+// TODO: add "script.js" to web_accessible_resources in manifest.json
+s.src = chrome.extension.getURL('jGravity-min.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
