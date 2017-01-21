2e903ef1c996ca504385fe2f17c1b3c4b5ab0cdc
Yun <yun@yun.sg>
2017-01-22 02:20:05 +0800

    insert record

--- web-starter-kit-master/php/getSummary.php
+++ web-starter-kit-master/php/getSummary.php
@@ -42,13 +42,33 @@ if ($cmd == 'register') {
 		while ( $row = $result->fetch_assoc() ) {
 			$output['record'][] = $row;
 		}
-
 	} else {
 		$output['status'] = "error";
 		$output['message'] = "no such user";
 	}
+} else if ($cmd == 'add') {
+	$user1 = $_POST['email1'];
+	$user2 = $_POST['email2'];
+	$amount = $_POST['amount'];
+	$uid1 = 0
+	$uid2 = 0;
 
+	$sql = "SELECT * FROM users WHERE email = '" . $db->escape_string($email1) . "'";
+	$result = $db->query($sql);
+	if ( $result->num_rows > 0 ) {
+		$user = $result->fetch_assoc();
+		$uid1 = $user["ID"];
+	}
 
+	$sql = "SELECT * FROM users WHERE email = '" . $db->escape_string($email2) . "'";
+	$result = $db->query($sql);
+	if ( $result->num_rows > 0 ) {
+		$user = $result->fetch_assoc();
+		$uid2 = $user["ID"];
+	}
+
+	$sql = "INSERT INTO records(user1, user2, amount) VALUES(" . $uid1 . ", " . $uid2 . ", " . $amount . ")";
+	$db->query($sql);
 }
 
 echo json_encode($output);
